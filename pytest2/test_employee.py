b580178112aa726673d7b000e16f00d410989ed2
Anjum269 <01fe24bca044.kletech@gmail.com>
2025-12-02 10:46:30 +0530

    Delete pytest2/test_employee.py

--- pytest2/test_employee.py
+++
@@ -1,9 +0,0 @@
-from employee import employee_details
-def test_employee_details():
-    expected_output = {
-        "Employee Name": "Alice",
-        "Employee ID": "E1001", 
-        "Department": "IT",
-        "Salary": 55000
-        }
-    assert employee_details("Alice", "E1001", "IT", 55000) == expected_output
